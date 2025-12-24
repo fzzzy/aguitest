f8aa2fb16ff92dd6b2e37882d1f89552ffcaea32
Donovan Preston <46048+fzzzy@users.noreply.github.com>
2025-12-24 08:57:09 -0500

    Chat input container

--- src/index.ts
+++ src/index.ts
@@ -68,7 +68,6 @@ function defineComponent(name: string) {
   });
 }
 
-defineComponent("chat-input-container");
 defineComponent("attachments-container");
 defineComponent("input-wrapper");
 defineComponent("mic-button");
